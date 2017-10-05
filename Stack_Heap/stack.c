fe02b1f7d6d3edd0539acd58311c291abfb74eb0
Paul5566 <eric20021232000@gmail.com>
2017-10-05 12:54:08 -0700

    add the address

--- Stack_Heap/stack.c
+++ Stack_Heap/stack.c
@@ -8,12 +8,16 @@
 void foo()
 {
 	int a[3] = {1,2,3};
+	int c[3] = {4,5,6};
+	printf("%p\n",&a);
+	printf("%p\n",&c);//first in ,first out!! "stack"?
 }
 
 void bar()
 {
-	int b[3];
-	printf("%d\t %d\t %d\t\n",b[0],b[1],b[2]);
+	int b[5];
+	printf("%p\n",&b);
+	printf("%p\t %p\t %p\t\n",&b[0],&b[1],&b[3]);
 }
 
 int main(void){

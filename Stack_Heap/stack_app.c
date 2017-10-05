225f8d74a1cb15c1c08520e2ff67a1174a106bb0
Paul5566 <eric20021232000@gmail.com>
2017-10-05 13:49:16 -0700

    sampel code for stack

---
+++ Stack_Heap/stack_app.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+/*
+
+first in and first out 
+
+function memory allocat the way is stack?? 
+
+so,what you change the variable it has been memory changed
+
+*/
+
+void foo()
+{
+	int a[3] = {1,2,3};
+}
+
+void bar()
+{
+	int b[3];
+	printf("%p\t %p\t %p\t\n",b[0],b[1],b[3]);
+}
+
+int main(void){
+	foo();
+	bar();
+}

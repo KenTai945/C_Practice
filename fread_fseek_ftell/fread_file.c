223ead8c47ca647f410606cb4185c515dcecb81b
paul5566 <eric20021232000@gmail.com>
2018-07-20 02:03:29 -0700

    (1) Modify the code by using the function to change the pointer fp to read file at the half of the socumnet
    (2) change the txt file to make the result more clear

--- fread_fseek_ftell/fread_file.c
+++ fread_fseek_ftell/fread_file.c
@@ -3,22 +3,47 @@
 
 int main(void)
 {
-	FILE *fp; 	
+	FILE *fp; 
 
-	char buffer[20];
+	size_t sz_fl;
 
-	fp = fopen("./filefread.txt","");
-	
-	fseek(fp, 0, SEEK_END);
+	//size_t medium_sz_fl;
+	char buffer[100];
 
-	size_t sz_fl;	//declare a variable to store the size of the file we are going to open
+	char buffer_less[50];
+
+	fp = fopen("fread_file.txt","r");	
+	if (fp == NULL)
+	{
+		printf("failed to fopen\n");
+		exit(EXIT_FAILURE);
+	}
+	/*
+        Don' forget to reset the position of fp                                   
+		use the function of fseak &ftell to calculate the size of the file       
+	*/ 
+	fseek(fp,0L,SEEK_END);
 	
 	sz_fl = ftell(fp);
-	fseek(fp, 0, SEEK_SET);	
+
+	rewind(fp);//  fseek(fp,0L,SEEK_SET)) ,seeking to go to the beginning
+	
 	fread(buffer,sz_fl,1,fp);
-	/*
-		printf("%s/n",buffer);
-	*/
-	fclose(fp);
+	
+	printf("printf the whole document\n");
+	printf("%s/n",buffer);
+
+	rewind(fp);
+	/* use the function of fseek to modify the code to read the last several line*/
+	
+	printf("\n printf the half of the document\n");
+	fseek(fp,-(sz_fl/2),SEEK_END); 
+	fread(buffer_less,sz_fl,1,fp);
+    
+    printf("%s/n",buffer_less);
+	
+	rewind(fp);
+	fclose(fp);//Don't Forget
+
 	return 0;
 }
--- fread_fseek_ftell/fseek_ftell_size.c
+++ fread_fseek_ftell/fseek_ftell_size.c
@@ -1,29 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int mian(void)
+int main(void)
 {
 	FILE *fp;
 
 	size_t fl_size;
 
-	fp = fopen("fread_file.txt","+r");
-	if (fp == NULL)
-	{
-		printf("Error Opening File\n");
-	}
-	/* use the fseek & ftell to calculate the size of the function)*/
+	fp = fopen("fread_file.txt","r");
 
-	fseek(fp,0,SEEK_END);
+	if (fp == NULL)
+    {
+        printf("failed to fopen \n");
+        exit(EXIT_FAILURE);
+    }
+
+	/* 
+		use the function fseak &ftell to calculate the size of the file 
+		Don' forget to reset the position of fp
+	*/
+
+	fseek(fp,0L,SEEK_END);
 
 	fl_size = ftell(fp);
 
-	rewind(fp);
-
+	rewind(fp);//  fseek(fp,0L,SEEK_SET)) ,seeking to go to the beginning
+	
 	printf(" the size of the file is %zu\n",fl_size);
-
+	
 	fclose(fp);//Don't Forget
 
 	return 0;
 }
-   
